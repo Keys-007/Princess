30156215e1b6ef3705eeb9c4e5001e5231a4ec6d
Akeno <82025071+Stella-80@users.noreply.github.com>
2021-06-11 15:31:15 +0530

    Update config.py

--- AkenoMusic/config.py
+++ AkenoMusic/config.py
@@ -20,4 +20,4 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
 ARQ_API_KEY = getenv("ARQ_API_KEY", "VKSSQH-SRNJYZ-MSOJAI-QALESR-ARQ")
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ? !").split())
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
+SUDO_USERS = 1461968113
