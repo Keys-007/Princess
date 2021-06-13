2ec700ca945d84239befe8fcfc8089558453969c
AkenoXProject <84738169+AkenoXProject@users.noreply.github.com>
2021-06-13 22:42:28 +0530

    Update config.py

--- AkenoMusic/config.py
+++ AkenoMusic/config.py
@@ -7,11 +7,11 @@ if os.path.exists("local.env"):
     load_dotenv("local.env")
 
 que = {}
-SESSION_NAME = "BQC0XDDR5JXq5HSWkbLzMCsL27429fva-DtyIgO-CWxVfUPjKn4l1CvCosUQvjdycvz6MXelyN_i2v3f8JJ0ZjNaUiIQl-6cDEhCmW0TGHeCI6RSyTpZ3JG8drrxl7Ja_ebKQq7TN8Wo2wlv1oXxm-qVhzmco4vBUM5y_6gXWuOWwELCWlMmIzuPMO-ooow-zONSJqs2hgK-KuBcABAaDWQwiNQjgU_QUKo2cfYbhpMBybKV22Kq81LH-eQWAV9oMUFKtOutvgx9UG695KMrK7nSI-kSmAQ_5L7OfwKKG9KyFsceschjdIkI7G3covbHYbHYYOitjLbbACgsqVejEIV1Q1H2wwA"
-BOT_TOKEN = "1700341854:AAGW_H-5wmLJZhmTbru0a0jRs2QfdpbHeTU"
-BOT_NAME = "akeno_robot"
+SESSION_NAME = "BQBWdj7KYmEHKaPmAWOvfR222fIklhP3L8DJk0-1vDsEqBX5j7w11d_UHOm3qKI7IKlsuw6uQQ5wmF2YZg1Fdl7NZGIEwPgF6H_CDuu9LqkE3N1OweWOX2LktzTJUzIsXftekyYi3nipgYwnXmGc6gtK2ZztVMkKVmv53rtKv3oVih88-IEqMg0uDt6cxFhmG1etSwv1cWSFgAug3IGd7CyNm8CI0v-rFHHm2zeT5g5don0GJcfoDyw9S1KeFTevpnPavAOReIRsEhEdkwxFKey7vmwM4GCemB8iTWUC_apme9mi1lo61S2KJ1Bc9ECoI7UnZscO-UO2YAc3nbiRkOvdW50AxgA"
+BOT_TOKEN = "1827610303:AAFQj58Dp8oduTKayyhEbzFoMbhcEcSaBuo"
+BOT_NAME = "princess_robot"
 UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "@akenoXsupport")
-BG_IMAGE =getenv("BG_IMAGE", "https://telegra.ph/file/d06f152fe1c7a9f5e98c4.jpg") 
+BG_IMAGE =getenv("BG_IMAGE", "https://telegra.ph/file/4867a4913b1d04c663850.jpg") 
 admins = {}
 API_ID = 2742026
 API_HASH = "f7a7f2fd4433c4a26355afa93a7e6f85"
