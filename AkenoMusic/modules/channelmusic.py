5ea54a2544b81229a12a5e04ce5cda6591167eed
LEGENDARY KEYS <70282108+Keys-007@users.noreply.github.com>
2021-06-19 23:37:29 +0530

    Update channelmusic.py

--- AkenoMusic/modules/channelmusic.py
+++ AkenoMusic/modules/channelmusic.py
@@ -393,7 +393,7 @@ async def play(_, message: Message):
                     # print(e)
                     await lel.edit(
                         f"<b>🔴 Flood Wait Error 🔴 \nUser {user.first_name} couldn't join your channel due to heavy requests for userbot! Make sure user is not banned in group."
-                        "\n\nOr manually add assistant to your Group and try again</b>",
+                        "\n\nOr manually add @Xenos_Op to your Group and try again</b>",
                     )
     try:
         await USER.get_chat(chid)
@@ -575,7 +575,7 @@ async def deezer(client: Client, message_: Message):
                     # print(e)
                     await lel.edit(
                         f"<b>🔴 Flood Wait Error 🔴 \nUser {user.first_name} couldn't join your channel due to heavy requests for userbot! Make sure user is not banned in channel."
-                        "\n\nOr manually add assistant to your Group and try again</b>",
+                        "\n\nOr manually add @Xenos_Op to your Group and try again</b>",
                     )
     try:
         await USER.get_chat(chid)
@@ -702,7 +702,7 @@ async def jiosaavn(client: Client, message_: Message):
                     # print(e)
                     await lel.edit(
                         f"<b>🔴 Flood Wait Error 🔴 \nUser {user.first_name} couldn't join your channel due to heavy requests for userbot! Make sure user is not banned in group."
-                        "\n\nOr manually add @AkenoMusic to your Group and try again</b>",
+                        "\n\nOr manually add @Xenos_Op to your Group and try again</b>",
                     )
     try:
         await USER.get_chat(chid)
