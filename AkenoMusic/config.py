aa39323122fb6346c21bc559b7417f2356cfa6f4
LEGENDARY KEYS <70282108+Keys-007@users.noreply.github.com>
2021-06-22 21:53:47 +0530

    Update config.py

--- AkenoMusic/config.py
+++ AkenoMusic/config.py
@@ -7,17 +7,16 @@ if os.path.exists("local.env"):
     load_dotenv("local.env")
 
 que = {}
-SESSION_NAME = "BQA8htAO_Z18aRkHdP1Q3MNDE_cXhqA8o4-V6vDEBa4KD8AElupEDSl2zQkhOhxu3QJkpy1shi2zmAVizGijq1VTxD2UzR9Dc1_xmRMeWve3rO4-TzPEZTw2IZxhahnCnX9D-5vUddixhpsJ2S8KLW9tsN-Uf1GhBONISmicRuZD8ZUZH1ToBjSvnxBwtahNqD7LrcvTmCQn4mRGDtnj8tIdB8BM_dsUab8TyJwpfWgb_9QTCWe8T7IythU8ertOlHnDEVfg-FC-S9XUlNfLuK4UWEb26hfVWkrvjVHNPP_xyWwQAShjRUDSfuX8q0wTlGDNfrqVNrNCNn8lx7MF9WMBW50AxgA"
-BOT_TOKEN = "1771297531:AAEcyxO9LjBwK8QYv0Nq8-Ibqimg6rTGXQ8"
-BOT_NAME = "HybridMusicRobot"
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "@akenoXsupport")
+SESSION_NAME = getenv("SESSION_NAME", "session")
+BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_NAME = getenv("BOT_NAME")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "HybridUpdates") 
 BG_IMAGE =getenv("BG_IMAGE", "https://telegra.ph/file/af1e8f26868716695766e.jpg")
 admins = {}
-API_ID = 3325295
-API_HASH = "c8e4335cb03e21e5abd2a12d01b67462"
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH")
 
-DURATION_LIMIT = getenv("DURATION_LIMIT", "7")
-ARQ_API_KEY = getenv("ARQ_API_KEY", "VKSSQH-SRNJYZ-MSOJAI-QALESR-ARQ")
-COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ? !").split())
-
-SUDO_USERS = 1603318426
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
+ARQ_API_KEY = getenv("ARQ_API_KEY", None)
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
+SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
